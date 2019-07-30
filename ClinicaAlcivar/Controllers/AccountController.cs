47a1c7f028fff6d203f32ffb0e3db7c36ee513af
Piedra, Jostin A.          Intl (Ecuador) <jostin.piedra@chubb.com>
2019-07-30 14:09:49 -0500

    Add project files.

---
+++ ClinicaAlcivar/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClinicaAlcivar.Models;
+
+namespace ClinicaAlcivar.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [HttpGet]
+        public ActionResult InicioSesion()
+        {
+            return View();
+        }
+        public ActionResult Verificar(Account model)
+        {
+            return View();
+        }
+    }
+}
---
+++ ClinicaAlcivar/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicaAlcivar.Models
+{
+    public class Account
+    {
+        public string Nombre { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
